867bf777b7b41e1b3bb46ac82ef1dcdee660b595
Wouter Deconinck <wdconinc@gmail.com>
2025-09-18 00:14:24 +0000

    Refactor database include guards and clean up unused code in database handling classes

--- Analysis/include/QwDatabase.h
+++ Analysis/include/QwDatabase.h
@@ -18,16 +18,18 @@
 #include <variant>
 
 // Third Party Headers
+#ifdef __USE_DATABASE__
 #include <sqlpp11/sqlpp11.h>
 #ifdef __USE_DATABASE_MYSQL__
 #include <sqlpp11/mysql/mysql.h>
-#endif
+#endif // __USE_DATABASE_MYSQL__
 #ifdef __USE_DATABASE_SQLITE3__
 #include <sqlpp11/sqlite3/sqlite3.h>
-#endif
+#endif // __USE_DATABASE_SQLITE3__
 #ifdef __USE_DATABASE_POSTGRESQL__
 #include <sqlpp11/postgresql/postgresql.h>
-#endif
+#endif // __USE_DATABASE_POSTGRESQL__
+#endif // __USE_DATABASE__
 
 // ROOT headers
 #include "TString.h"
