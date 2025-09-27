3aa257c5b4e995b13b11deab00b4b0cbf2ddd158
Wouter Deconinck <wdconinc@gmail.com>
2025-09-27 12:03:19 -0500

    Modernize virtual functions for rntuple as well

--- Parity/include/QwMollerDetector.h
+++ Parity/include/QwMollerDetector.h
@@ -134,8 +134,8 @@ class QwMollerDetector:
 
     // RNTuple methods
 #ifdef HAS_RNTUPLE_SUPPORT
-    void ConstructNTupleAndVector(std::unique_ptr<ROOT::RNTupleModel>& model, TString& prefix, std::vector<Double_t>& values, std::vector<std::shared_ptr<Double_t>>& fieldPtrs);
-    void FillNTupleVector(std::vector<Double_t>& values) const;
+    void ConstructNTupleAndVector(std::unique_ptr<ROOT::RNTupleModel>& model, TString& prefix, std::vector<Double_t>& values, std::vector<std::shared_ptr<Double_t>>& fieldPtrs) override;
+    void FillNTupleVector(std::vector<Double_t>& values) const override;
 #endif // HAS_RNTUPLE_SUPPORT
 
     Bool_t Compare(VQwSubsystem *source);
