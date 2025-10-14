28f04960081e3768ee4f97dcbf6cc72697c786f9
Wouter Deconinck <wdconinc@gmail.com>
2025-10-14 05:12:57 +0000

    Make CopyFrom non-virtual in VQwDataElement and VQwHardwareChannel
    Remove the virtual specifier from the CopyFrom declarations so these are
    regular member functions rather than virtual methods. This clarifies intent
    and avoids unintended virtual dispatch for the CopyFrom implementations. It also removes recurring warnings about the hiding of virtuals by overloading them.

--- Analysis/include/VQwHardwareChannel.h
+++ Analysis/include/VQwHardwareChannel.h
@@ -44,7 +44,7 @@ public:
   VQwHardwareChannel(const VQwHardwareChannel& value, VQwDataElement::EDataToSave datatosave);
   ~VQwHardwareChannel() override { };
 
-  virtual void CopyFrom(const VQwHardwareChannel& value);
+  void CopyFrom(const VQwHardwareChannel& value);
 
   void ProcessOptions();
 
